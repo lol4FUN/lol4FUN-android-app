701fbfe8307dbcbe49fae987d8f5c3fd6783a45e
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-19 21:18:22 -0300

    created constants to firebase fields and db

--- app/src/main/java/com/github/lol4fun/util/ConstantsUtil.kt
+++ app/src/main/java/com/github/lol4fun/util/ConstantsUtil.kt
@@ -5,4 +5,17 @@ class ConstantsUtil {
     object Main {
         const val RC_SIGN_IN = 123
     }
+
+    object FirestoreDataBaseNames{
+        const val DATABASE_CUSTOMERS = "customers"
+    }
+
+    object FirestoreDataBaseFields{
+        const val FIELD_USER_NAME = "name"
+        const val FIELD_USER_EMAIL = "email"
+        const val FIELD_USER_PREMIUM = "premiumUser"
+        const val FIELD_USER_COLOR_PREFERENCE = "colorPreference"
+        const val FIELD_USER_COLOR_PREFERENCE_DARK = 1
+        const val FIELD_USER_COLOR_PREFERENCE_LIGHT = 2
+    }
 }
