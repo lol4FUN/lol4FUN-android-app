38fa5346e001d6e420b7bb2d1155258d42de8683
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-21 10:42:04 -0300

    added interceptor to header

--- app/src/main/java/com/github/lol4fun/core/api/HeaderInterceptor.kt
+++ app/src/main/java/com/github/lol4fun/core/api/HeaderInterceptor.kt
@@ -1,2 +1,17 @@
 package com.github.lol4fun.core.api
 
+import com.github.lol4fun.util.ConstantsUtil.Api.HEADER_RIOT_TOKEN_NAME
+import com.github.lol4fun.util.ConstantsUtil.Api.HEADER_RIOT_TOKEN_VALUE
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class HeaderInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response = chain.run {
+        proceed(
+            request()
+                .newBuilder()
+                .addHeader(HEADER_RIOT_TOKEN_NAME, HEADER_RIOT_TOKEN_VALUE)
+                .build()
+        )
+    }
+}
