1ee152c160c5cb22ab943228b223849093911c7b
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-20 22:24:13 -0300

    added new method to get user from firebase

--- app/src/main/java/com/github/lol4fun/core/repository/home/MainRepository.kt
+++ app/src/main/java/com/github/lol4fun/core/repository/home/MainRepository.kt
@@ -1,11 +1,13 @@
 package com.github.lol4fun.core.repository.home
 
+import com.github.lol4fun.core.model.Customer
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_COLOR_PREFERENCE
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_COLOR_PREFERENCE_DARK
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_EMAIL
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_NAME
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_PREMIUM
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseNames.DATABASE_CUSTOMERS
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
@@ -32,5 +34,15 @@ class MainRepository {
         ).set(
             newUser
         )
+    }
+
+    fun getUserFirestore(): Customer? {
+        val docRef = FirebaseFirestore.getInstance()
+            .collection(DATABASE_CUSTOMERS)
+            .document(FirebaseAuth.getInstance().currentUser?.uid ?: "")
+
+        val result = Tasks.await(docRef.get())
+
+        return result.toObject(Customer::class.java)
     }
 }
