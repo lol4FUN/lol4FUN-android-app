d1f2591c1f3a3350e7089f84a34f15d52d61f076
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-20 22:22:41 -0300

    added new model of Customer

---
+++ app/src/main/java/com/github/lol4fun/core/model/Customer.kt
@@ -0,0 +1,11 @@
+package com.github.lol4fun.core.model
+
+data class Customer (
+    var name: String? = null,
+    var email: String? = null,
+    var summonerName: String? = null,
+    var accountId: String? = null,
+    var premiumUser: Boolean? = null,
+    var profilePicture: String? = null,
+    var colorPreference: Long? = null
+)
