5684ee62d1d3717ee3826d628e07ddda68a023e4
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-21 10:45:37 -0300

    constants updated

--- app/src/main/java/com/github/lol4fun/util/ConstantsUtil.kt
+++ app/src/main/java/com/github/lol4fun/util/ConstantsUtil.kt
@@ -17,5 +17,15 @@ class ConstantsUtil {
         const val FIELD_USER_COLOR_PREFERENCE = "colorPreference"
         const val FIELD_USER_COLOR_PREFERENCE_DARK = 1
         const val FIELD_USER_COLOR_PREFERENCE_LIGHT = 2
+        const val FIELD_USER_SUMMONER_NAME = "summonerName"
+        const val FIELD_USER_REGION = "region"
+        const val FIELD_USER_REGION_BR = "br1"
+    }
+
+    object Api {
+        //todo modularizar com link para regiao do usuário, hoje está apenas para o BR
+        const val BASE_URL = "https://br1.api.riotgames.com/lol/"
+        const val HEADER_RIOT_TOKEN_NAME = "X-Riot-Token"
+        const val HEADER_RIOT_TOKEN_VALUE = "RGAPI-d566318f-c2c7-4dc8-b101-ea4190c4b184"
     }
 }
