a1622379c35df5a8d0371cdbfd07be40c59cd10c
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-20 23:44:53 -0300

    customer model updated

--- app/src/main/java/com/github/lol4fun/core/model/Customer.kt
+++ app/src/main/java/com/github/lol4fun/core/model/Customer.kt
@@ -5,6 +5,7 @@ data class Customer (
     var email: String? = null,
     var summonerName: String? = null,
     var accountId: String? = null,
+    var id: String? = null,
     var premiumUser: Boolean? = null,
     var profilePicture: String? = null,
     var colorPreference: Long? = null
