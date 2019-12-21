f7d7253a25e078be1e61f7142befcacb848b0084
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-21 11:44:58 -0300

    interceptor code updated

--- app/src/main/java/com/github/lol4fun/core/api/ApiService.kt
+++ app/src/main/java/com/github/lol4fun/core/api/ApiService.kt
@@ -11,8 +11,9 @@ object ApiService {
     val riotApi: RiotApi = getRiotApiClient().create(RiotApi::class.java)
 
     fun getRiotApiClient() : Retrofit {
-        val okHttpClient = OkHttpClient()
-        okHttpClient.interceptors().add(HeaderInterceptor())
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(HeaderInterceptor())
+            .build()
 
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
