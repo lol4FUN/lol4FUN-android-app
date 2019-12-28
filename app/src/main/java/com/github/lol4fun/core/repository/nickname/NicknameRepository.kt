f1e4afbf8bf103f65321390380832c266f998771
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-28 20:36:07 -0300

    finish call nickname repository

--- app/src/main/java/com/github/lol4fun/core/repository/nickname/NicknameRepository.kt
+++ app/src/main/java/com/github/lol4fun/core/repository/nickname/NicknameRepository.kt
@@ -3,6 +3,7 @@ package com.github.lol4fun.core.repository.nickname
 import com.github.lol4fun.core.api.Resource
 import com.github.lol4fun.core.model.SummonerInfo
 import com.github.lol4fun.core.repository.BaseRepository
+import com.github.lol4fun.util.ConstantsUtil
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_REGION
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_REGION_BR
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_RIOT_ACCOUNT_ID
@@ -10,7 +11,11 @@ import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_RIOT_PROFILE_ICON_ID
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_SUMMONER_NAME
 import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseNames.DATABASE_CUSTOMERS
+import com.github.lol4fun.util.ErrorUtils
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.firestore.FirebaseFirestoreException
 import java.util.*
+import java.util.concurrent.ExecutionException
 
 class NicknameRepository : BaseRepository() {
 
@@ -20,8 +25,8 @@ class NicknameRepository : BaseRepository() {
         )
     }
 
-    fun saveSummonerInfoAtFirestore(summonerInfo: SummonerInfo) {
-        db.collection(DATABASE_CUSTOMERS)
+    fun saveSummonerInfoAtFirestore(summonerInfo: SummonerInfo): Resource {
+        val taskSaveSummonerInfo = db.collection(DATABASE_CUSTOMERS)
             .document(auth.uid ?: UUID.randomUUID().toString())
             .update(
                 mapOf(
@@ -31,14 +36,21 @@ class NicknameRepository : BaseRepository() {
                     FIELD_USER_RIOT_ID to summonerInfo.id,
                     FIELD_USER_RIOT_PROFILE_ICON_ID to summonerInfo.profileIconId
                 )
-            ).addOnCompleteListener {
-                if (it.isSuccessful) {
-                    //todo retornar sucesso
-                } else {
-                    //todo retornar erro
-                }
+            )
+        return try {
+            Tasks.await(taskSaveSummonerInfo)
+
+            Resource.success(null)
+        } catch (executionException: ExecutionException) {
+            val error = ErrorUtils.parseError(executionException.cause as FirebaseFirestoreException)
+
+            error?.message?.let {  message ->
+                Resource.error(message)
+            } ?: run {
+                Resource.error(ConstantsUtil.Error.ERROR_DEFAULT)
             }
+        } catch (interruptedException: InterruptedException) {
+            Resource.error(ConstantsUtil.Error.ERROR_DEFAULT)
+        }
     }
-
-
 }
