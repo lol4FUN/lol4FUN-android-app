e5f492d4a0f4edccd189fafbc645569238d4ed69
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-29 13:28:14 -0300

    created repository to the champions module

---
+++ champions/src/main/java/com/github/champions/repository/ChampionsRepository.kt
@@ -0,0 +1,15 @@
+package com.github.champions.repository
+
+import com.github.lol4fun.core.api.Resource
+import com.github.lol4fun.core.repository.BaseRepository
+
+class ChampionsRepository : BaseRepository() {
+
+    suspend fun getChampions(): Resource {
+        return safeApiCall(
+            call = { apiDDragon.getChampionsAsync() }
+        )
+    }
+
+
+}
