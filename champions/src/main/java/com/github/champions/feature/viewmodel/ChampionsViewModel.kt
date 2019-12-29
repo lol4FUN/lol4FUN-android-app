d4ae87c58cffd90ab4025ff78f629026e0f1d157
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-29 13:27:36 -0300

    created viewmodel to the champions module

---
+++ champions/src/main/java/com/github/champions/feature/viewmodel/ChampionsViewModel.kt
@@ -0,0 +1,32 @@
+package com.github.champions.feature.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.champions.feature.business.ChampionsBusiness
+import com.github.champions.feature.listener.ChampionsListener
+import com.github.lol4fun.core.model.Champions
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ChampionsViewModel : ViewModel(), ChampionsListener {
+
+    private val business: ChampionsBusiness by lazy { ChampionsBusiness(this) }
+
+    val onErrorGetChampionsListLiveData: MutableLiveData<Int> = MutableLiveData()
+    val onSuccessGetChampionsListLiveData: MutableLiveData<Champions> = MutableLiveData()
+
+    fun getChampions() {
+        viewModelScope.launch(Dispatchers.IO) {
+            business.getChampions()
+        }
+    }
+
+    override fun onErrorGetChampionsList(resourceId: Int) {
+        onErrorGetChampionsListLiveData.postValue(resourceId)
+    }
+
+    override fun onSuccessGetChampionsList(champions: Champions?) {
+        onSuccessGetChampionsListLiveData.postValue(champions)
+    }
+}
