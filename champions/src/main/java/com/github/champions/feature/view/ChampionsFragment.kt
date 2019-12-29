5aa040ca6f223d4ce0f0fc97d3f790ff08563b93
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-29 13:26:41 -0300

    champions fragment updated

--- champions/src/main/java/com/github/champions/feature/view/ChampionsFragment.kt
+++ champions/src/main/java/com/github/champions/feature/view/ChampionsFragment.kt
@@ -1,12 +1,19 @@
-package com.github.champions
+package com.github.champions.feature.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.github.champions.R
+import com.github.champions.feature.viewmodel.ChampionsViewModel
+import com.github.lol4fun.extensions.showSnackBar
+import kotlinx.android.synthetic.main.fragment_champions.*
 
 class ChampionsFragment: Fragment() {
+
+    private val viewModel: ChampionsViewModel by lazy { ChampionsViewModel() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -14,5 +21,29 @@ class ChampionsFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_champions, container, false)
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getChampions()
+        setupObservables()
+    }
+
+    private fun setupObservables() {
+        viewModel.onErrorGetChampionsListLiveData.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                this.context?.showSnackBar(pbChampionsLoading, it)
+            }
+        })
+
+        viewModel.onSuccessGetChampionsListLiveData.observe(viewLifecycleOwner, Observer {
+            it?.let { champions ->
+                champions.data.Aatrox.name
+            }
+        })
     }
 }
