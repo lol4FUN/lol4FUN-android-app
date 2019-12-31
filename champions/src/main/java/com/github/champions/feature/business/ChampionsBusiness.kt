88705a2f56b2d020b6e7df61acce53d9c2f4bc5f
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-30 21:03:41 -0300

    new setup of the champions list on business

--- champions/src/main/java/com/github/champions/feature/business/ChampionsBusiness.kt
+++ champions/src/main/java/com/github/champions/feature/business/ChampionsBusiness.kt
@@ -4,6 +4,7 @@ import com.github.champions.R
 import com.github.champions.feature.listener.ChampionsListener
 import com.github.champions.repository.ChampionsRepository
 import com.github.lol4fun.core.api.Status
+import com.github.lol4fun.core.model.Champion
 import com.github.lol4fun.core.model.Champions
 import com.github.lol4fun.extensions.serializeToMap
 
@@ -23,7 +24,13 @@ class ChampionsBusiness(
             Status.SUCCESS -> {
                 val championsMap = (resultGetChampions.data as? Champions)?.data.serializeToMap()
 
-                championsListener.onSuccessGetChampionsList(championsMap)
+                val championsList = arrayListOf<Any>()
+
+                championsMap.forEach {
+                    championsList.add(it.value)
+                }
+
+                championsListener.onSuccessGetChampionsList(championsList)
             }
         }
     }
