9803f70feb3d9c37a27c3ff0397501d1341d8d4b
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-12-30 21:05:38 -0300

    created adapter to champions recycler view

---
+++ champions/src/main/java/com/github/champions/adapter/ChampionsAdapter.kt
@@ -0,0 +1,37 @@
+package com.github.champions.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.github.champions.R
+import com.github.lol4fun.extensions.toChampionObject
+import kotlinx.android.synthetic.main.item_champion.view.*
+
+class ChampionsAdapter(
+    private val context: Context?,
+    private val championsList: ArrayList<Any>
+) : RecyclerView.Adapter<ChampionsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_champion, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return championsList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(context, championsList[position])
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(context: Context?, any: Any?) = with(itemView) {
+            val champion = any.toChampionObject()
+            itemView.rvChampionItemName.text = champion.name
+        }
+    }
+}
