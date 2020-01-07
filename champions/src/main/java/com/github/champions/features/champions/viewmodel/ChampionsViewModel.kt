696ea82af3a0353a7101a0f811598576a612bea8
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-01-07 20:49:39 -0300

    added inject to viewmodel

--- champions/src/main/java/com/github/champions/features/champions/viewmodel/ChampionsViewModel.kt
+++ champions/src/main/java/com/github/champions/features/champions/viewmodel/ChampionsViewModel.kt
@@ -1,16 +1,18 @@
 package com.github.champions.features.champions.viewmodel
 
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.champions.features.champions.business.ChampionsBusiness
 import com.github.champions.features.champions.listener.ChampionsListener
+import com.github.lol4fun.core.base.BaseViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.koin.core.inject
+import org.koin.core.parameter.parametersOf
 
-class ChampionsViewModel : ViewModel(), ChampionsListener {
+class ChampionsViewModel : BaseViewModel(), ChampionsListener {
 
-    private val business: ChampionsBusiness by lazy { ChampionsBusiness(this) }
+    private val business: ChampionsBusiness by inject { parametersOf(this) }
 
     val onErrorGetChampionsListLiveData: MutableLiveData<Int> = MutableLiveData()
     val onSuccessGetChampionsListLiveData: MutableLiveData<ArrayList<Any>> = MutableLiveData()
