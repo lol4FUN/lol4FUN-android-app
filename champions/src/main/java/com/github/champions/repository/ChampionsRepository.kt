bda95032dd4378656a248a19806c31eb873ddec0
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-01-07 08:57:35 -0300

    path of base repository updated

--- champions/src/main/java/com/github/champions/repository/ChampionsRepository.kt
+++ champions/src/main/java/com/github/champions/repository/ChampionsRepository.kt
@@ -1,7 +1,7 @@
 package com.github.champions.repository
 
 import com.github.lol4fun.core.api.Resource
-import com.github.lol4fun.core.repository.BaseRepository
+import com.github.lol4fun.core.base.BaseRepository
 
 class ChampionsRepository : BaseRepository() {
 
