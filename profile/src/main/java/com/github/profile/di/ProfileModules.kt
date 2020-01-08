a5d6489b10a2b833ed7da24887209d80d262b8d7
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-01-07 22:02:00 -0300

    added di to modules

--- profile/src/main/java/com/github/profile/di/ProfileModules.kt
+++ profile/src/main/java/com/github/profile/di/ProfileModules.kt
@@ -1,15 +1,18 @@
 package com.github.profile.di
 
+import com.github.profile.feature.business.ProfileBusiness
+import com.github.profile.feature.viewmodel.ProfileViewModel
+import com.github.profile.repository.ProfileRepository
 import org.koin.dsl.module
 
 val businessModule = module {
-
+    single { ProfileBusiness() }
 }
 
 val repositoryModule = module {
-
+    single { ProfileRepository() }
 }
 
 val viewModelModule = module {
-
+    single { ProfileViewModel() }
 }
