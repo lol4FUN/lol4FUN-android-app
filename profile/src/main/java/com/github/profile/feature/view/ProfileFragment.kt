86e2e34632bd7bb0c96d40362f29ebec84afc0c1
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-01-07 22:01:35 -0300

    change path of the ProfileFragment

--- profile/src/main/java/com/github/profile/feature/view/ProfileFragment.kt
+++ profile/src/main/java/com/github/profile/feature/view/ProfileFragment.kt
@@ -1,18 +1,22 @@
-package com.github.profile
+package com.github.profile.feature.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.profile.R
+import com.github.profile.di.ProfileDependencyInjection
 
-class ProfileFragment: Fragment() {
+class ProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        ProfileDependencyInjection.injectModules()
+
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 }
