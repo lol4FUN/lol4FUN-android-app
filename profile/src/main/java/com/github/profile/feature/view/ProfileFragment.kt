d38e3e401b4f993abd7cd97604e35f555d2957f7
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-01-13 08:20:56 -0300

    profile screen almost finished

--- profile/src/main/java/com/github/profile/feature/view/ProfileFragment.kt
+++ profile/src/main/java/com/github/profile/feature/view/ProfileFragment.kt
@@ -1,14 +1,20 @@
 package com.github.profile.feature.view
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.github.lol4fun.core.model.Vi
+import com.github.lol4fun.util.ConstantsUtil.Api.BASE_URL_PROFILE_ICON
+import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_COLOR_PREFERENCE_DARK
+import com.github.lol4fun.util.GlideApp
 import com.github.profile.R
 import com.github.profile.di.ProfileDependencyInjection
 import com.github.profile.feature.viewmodel.ProfileViewModel
+import kotlinx.android.synthetic.main.fragment_profile.*
 import org.koin.android.ext.android.inject
 
 class ProfileFragment : Fragment() {
@@ -37,8 +43,19 @@ class ProfileFragment : Fragment() {
 
     private fun setupObservables() {
         viewModel.onSuccessGetUserFirestore.observe(viewLifecycleOwner, Observer {
+            vgProfileContent.visibility = View.VISIBLE
+            pbProfileLoading.visibility = View.GONE
+
             it?.let {
-                //todo fill user data
+                GlideApp
+                    .with(this)
+                    .load("$BASE_URL_PROFILE_ICON${it.profileIconId}.png")
+                    .into(civProfileSummonerIcon)
+
+                etProfileSummonerName.text = Editable.Factory.getInstance().newEditable(it.summonerName)
+                etProfileName.text = Editable.Factory.getInstance().newEditable(it.name)
+                tilProfileEmail.editText?.text = Editable.Factory.getInstance().newEditable(it.email)
+                swProfileSystemColorPreference.isChecked = it.colorPreference == FIELD_USER_COLOR_PREFERENCE_DARK
             }
         })
     }
