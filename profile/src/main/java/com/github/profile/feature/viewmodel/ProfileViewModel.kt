de7f277a7861a25badd945ca53b224eb8ef1f5d8
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-01-14 09:03:45 -0300

    updated ProfileViewModel with listener and live data

--- profile/src/main/java/com/github/profile/feature/viewmodel/ProfileViewModel.kt
+++ profile/src/main/java/com/github/profile/feature/viewmodel/ProfileViewModel.kt
@@ -4,14 +4,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.github.lol4fun.core.base.BaseViewModel
 import com.github.lol4fun.core.model.Customer
+import com.github.profile.R
 import com.github.profile.feature.business.ProfileBusiness
+import com.github.profile.feature.listener.ProfileListener
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.koin.core.inject
+import org.koin.core.parameter.parametersOf
 
-class ProfileViewModel : BaseViewModel() {
+class ProfileViewModel : BaseViewModel(), ProfileListener {
 
-    private val business: ProfileBusiness by inject()
+    private val business: ProfileBusiness by inject{ parametersOf(this) }
+
+    val onFailureSaveInfoData: MutableLiveData<Int> = MutableLiveData()
+    val onSuccessSaveInfoData: MutableLiveData<Int> = MutableLiveData()
 
     val onSuccessGetUserFirestore: MutableLiveData<Customer> = MutableLiveData()
 
@@ -19,6 +25,20 @@ class ProfileViewModel : BaseViewModel() {
         viewModelScope.launch(Dispatchers.IO) {
             onSuccessGetUserFirestore.postValue(business.getUserFirestore())
         }
+    }
+
+    fun saveUserInfo(hashMapProfile: HashMap<String, Any>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            business.saveUserInfo(hashMapProfile)
+        }
+    }
+
+    override fun onFailureSaveUserData() {
+        onFailureSaveInfoData.postValue(R.string.error_saving_user_data)
+    }
+
+    override fun onSuccessSaveUserData() {
+        onSuccessSaveInfoData.postValue(R.string.data_saved_successfully)
     }
 
 }
