bbeea6348a967fc0808c9599d39f3618b3937ee4
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-01-26 22:49:31 -0300

    deleted imports useless

--- app/src/main/java/com/github/lol4fun/core/repository/main/MainRepository.kt
+++ app/src/main/java/com/github/lol4fun/core/repository/main/MainRepository.kt
@@ -1,12 +1,5 @@
 package com.github.lol4fun.core.repository.main
 
 import com.github.lol4fun.core.base.BaseRepository
-import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_COLOR_PREFERENCE
-import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_COLOR_PREFERENCE_DARK
-import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_EMAIL
-import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_NAME
-import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseFields.FIELD_USER_PREMIUM
-import com.github.lol4fun.util.ConstantsUtil.FirestoreDataBaseNames.DATABASE_CUSTOMERS
-import java.util.*
 
 class MainRepository : BaseRepository()
